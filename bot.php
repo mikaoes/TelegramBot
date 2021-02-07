124e9c8414f46c1b8835ea91a290f05008e4c363
mikaoes <mail@mikaoesterreich.de>
2021-02-07 12:27:50 +0100

    Added GetUpdates API support

--- bot.php
+++ bot.php
@@ -1,5 +1,13 @@
 <?php
+# Necessary
 $token = "YOUR_BOT_TOKEN";
+$chatid = 0;
+$text = "";
 require 'telegrambot.php';
+
+# Examples
 send("YOUR_BOT_ID", "YOUR_MESSAGE");
+getupdates();
+echo $chatid;
+echo $text;
 ?>
--- telegrambot.php
+++ telegrambot.php
@@ -2,7 +2,18 @@
 function send($chatid, $message) {
   $message = str_replace(" ", "%20", $message);
   $url = "https://api.telegram.org/bot" . $GLOBALS['token'] . "/sendMessage?chat_id=" . $chatid . "&text=" . $message;
-  echo $url;
   file_get_contents($url);
+};
+function getupdates() {
+  $url = "https://api.telegram.org/bot" . $GLOBALS['token'] . "/getUpdates";
+  $get = file_get_contents($url);
+  $array = json_decode($get, true);
+  $array = end(end(end($array)));
+  print_r($array);
+  $GLOBALS['text'] = $array['text'];
+  $GLOBALS['chatid'] = $array['from']['id'];
+  echo "<br><br>";
+  echo $GLOBALS['text'] . " " . $GLOBALS['chatid'];
+#  echo json_encode($array);
 };
 ?>
